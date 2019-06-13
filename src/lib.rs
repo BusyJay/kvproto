0470ba91d8f76f28e4fb47453b480b4cfe2612bc
Jay Lee <busyjaylee@gmail.com>
2019-06-13 14:50:25 +0800

    add bench case

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#![feature(test)]
+
+extern crate test;
+
 #[cfg(feature = "protobuf-codec")]
 mod protobuf;
 mod text;
@@ -7,3 +11,27 @@ pub use crate::protobuf::*;
 
 #[cfg(feature = "prost-codec")]
 pub use crate::prost::*;
+
+pub mod tests {
+    use test::Bencher;
+    use crate::raft_cmdpb::RaftCmdResponse;
+
+    fn verify_current_term(msg: RaftCmdResponse) {
+        assert_eq!(msg.get_header().get_current_term(), 2);
+    }
+
+    pub fn clone_and_move(msg: &RaftCmdResponse) {
+        let m = msg.clone();
+        verify_current_term(m);
+    }
+
+    #[bench]
+    fn bench_move_raft_cmd_response(b: &mut Bencher) {
+        let mut msg = RaftCmdResponse::new();
+        msg.mut_header().set_current_term(2);
+        b.iter(|| {
+            clone_and_move(&msg);
+        });
+    }
+}
+
